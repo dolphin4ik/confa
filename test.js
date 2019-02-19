21c843b50c46a2ecdc987b0fbe67f05fb6664611
Denis Zhirkov <denyzhirkov@yandex.ru>
2019-02-20 00:14:23 +0300

    feat: test vol.1

---
+++ test.js
@@ -0,0 +1,21 @@
+const confa = require('./index');
+
+confa.add('var1', 'Value-1', {
+  dev: 'Value-2',
+  prod: 'Value-3'
+});
+confa.add('A', {a: 'a'}, {
+  dev: {a: 'aa'},
+  prod: {a: 'aaa'}
+});
+confa.add('B', 115, {
+  dev: 116,
+  prod: 117
+});
+confa.add('C', 'C2H5OH', {
+  dev: 'elop'
+});
+
+console.log(confa.run());
+console.log(confa.run('dev'));
+console.log(confa.run('prod'));
