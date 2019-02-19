8d452170a0c0e61d458537cf7671c61f3c47e454
Denis Zhirkov <denyzhirkov@yandex.ru>
2019-02-20 00:13:30 +0300

    initial

---
+++ index.js
@@ -0,0 +1,27 @@
+const KEYS = {};
+
+module.exports = {
+  add(key, defaultValue, extension = {}) {
+
+    if(typeof key !== 'string') {
+      throw new Error(`key must be a String`)
+    }
+
+    if(defaultValue === undefined) {
+      throw new Error(`default value must be provided`)
+    }
+
+    KEYS[key] = {
+      ...extension,
+      "default": defaultValue
+    }
+  },
+
+  run(environment = "default") {
+    const result = {}
+    Object.keys(KEYS).forEach(k => {
+      result[k] = KEYS[k][environment] || KEYS[k]['default'];
+    });
+    return result;
+  }
+}
